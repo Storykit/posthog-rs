9e177a995460a4c3bbfa98449778ae0c7010828e
Karin <karin.reidarman@storykit.io>
2023-12-20 10:04:39 +0100

    expose Client and client types

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ mod public_api;
 /// Data types related to the API
 pub mod types;
 
-pub use client::ClientBuilder;
+pub use client::{Client, ClientBuilder, PrivateClient, PublicClient};
 pub use event::Event;
 pub use feature_flags::FeatureFlagsAPI;
 pub use public_api::PublicAPI;
